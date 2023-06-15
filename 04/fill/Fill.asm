59d109ed03c81dc510dc3be1e054805e72772007
Nikola <nikolaanicic99@gmail.com>
2023-06-16 00:52:05 +0200

    added the code for the porject in week 4, implementing multiplication and some keyboard and screen work in hack assembly

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,79 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+@i
+M = 0   
+
+@status
+M = 0
+
+@ARG
+M = 0
+
+
+(LOOP)
+
+    @POLL_KBD
+    0;JMP
+
+    (CHECK_STATUS)
+        @ARG
+        D = M
+        @status
+        D = D - M
+        // checks if the screen should be filled with 
+        @LOOP       // a diff colour 
+        D;JEQ
+
+    @ARG
+    D = M
+    @status
+    M=D  
+    @8192
+    D=A
+    @SCREEN
+    D = A + D
+    @i
+    M = D
+
+    (DRAW_LOOP)
+        @i     // set the pointer address
+        D = M  // load the pointer
+        D = D - 1 // decrease the pointer
+        M=D       // save the pointer to memory
+        @16383    // load the constant @SCREEN-1
+        D=A  
+        @i     
+        D = M - D // check if the drawing is done
+        @LOOP
+        D;JLE     
+
+        @status  // load the current screen status
+        D = M    
+        @i
+        A = M  // set the current pointer address and set the pixel
+        M = D
+        @DRAW_LOOP
+        0;JMP
+        
+
+(POLL_KBD)
+    @KBD
+    D = M
+    @SET_STATUS_BLACK
+    D;JNE
+    @SET_STATUS_WHITE
+    D;JEQ
+
+(SET_STATUS_BLACK)
+    @ARG
+    M = -1
+    @CHECK_STATUS
+    0;JMP
+
+(SET_STATUS_WHITE)
+    @ARG
+    M = 0
+    @CHECK_STATUS
+    0;JMP
--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -10,3 +10,46 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+@0
+D = M
+@i
+M = D
+@1
+D = M
+@j
+M = D
+@sum
+M = 0
+@i
+D = M
+@DONE
+D;JEQ
+@j
+D = M
+@DONE
+D;JEQ
+(ADD)
+    @i // set the address to variable i
+    D = M // load the data from memory to the register D
+    @DONE
+    D;JEQ // if the counter is zero it means the multiplication is done
+    D = D - 1 // decrease the counter
+    @i
+    M = D // store the new counter value back to memory
+    @sum // load the value of the sum variable
+    D=M // load the value into the register
+    @j // set the addres value to the j variable
+    D = D + M // add the variable to the sum
+    @sum // store the sum back to memory    
+    M=D
+    @ADD
+    0;JMP
+(DONE)
+    @sum
+    D = M
+    @2
+    M = D
+    @DONE
+    0;JMP
+
